09f34dea4a69c06abecf2e43c9ef31cef3f7073e
rafi <rafiharlian@gmail.com>
2020-04-18 15:14:30 +0700

    tugas sting sudah selesai pak

---
+++ countsentence.js
@@ -0,0 +1,21 @@
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word.substring(0, 3);
+var secondWord = word.substring(4,14); // do your own!
+var thirdWord = word.substring(15,17); // do your own!
+var fourthWord = word.substring(18,20); // do your own!
+var fifthWord = word.substring(21,25); // do your own!
+
+
+var firstWordLength = exampleFirstWord.length;
+var secondWordLength = secondWord.length;
+var thirdWordLength = thirdWord.length;
+var fourthWordLength = fourthWord.length;
+var fifthWordLength = fifthWord.length;
+
+// create new variables around here
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord +', with lenght: '+ secondWordLength);
+console.log('Third Word: ' + thirdWord +', with lenght: '+ thirdWordLength);
+console.log('Fourth Word: ' + fourthWord +', with lenght: '+ fourthWordLength);
+console.log('Fifth Word: ' + fifthWord +', with lenght: '+ fifthWordLength);
